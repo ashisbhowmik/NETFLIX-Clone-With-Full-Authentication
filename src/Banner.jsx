07e01f091096cc86c47d4dd74220f41e77c59f93
Ashis Bhowmik <ashisbhowmikisp@gmail.com>
2021-07-15 23:02:34 +0530

    react-awasome-loader-added

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,62 +1,51 @@
 import React, { useState, useEffect } from "react";
+import BannerHeader from "./BannerHeader";
+import { WifiLoader } from "react-awesome-loaders";
 import "./banner.css";
-import axios from "./axios";
-import request from "./request";
 
 const Banner = () => {
-  const [randomMovie, setRandomMovie] = useState();
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
-    const fetchRandomData = async () => {
-      const response = await axios.get(request.fetchNetflixOriginals);
-      //   get a random movie by math.floor and math.random function
-      // Math.floor(Math.random() * response.data.results.length - 1);
-      setRandomMovie(
-        response.data.results[
-          Math.floor(Math.random() * response.data.results.length - 1)
-        ]
-      );
-      // return response;
-    };
-    fetchRandomData();
+    setLoading(true);
   }, []);
 
-  //   string truncate function
-  function truncate(str, n) {
-    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
+  setTimeout(() => {
+    setLoading(false);
+  }, 5000);
 
   return (
-    <header
-      className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${randomMovie?.backdrop_path}")`,
-      }}
-    >
-      {/* this header has a background image and the below method will applied in uppon the image */}
-      <div className="banner__contents">
-        {/* title */}
-        <h1 className="banner__title">
-          {randomMovie?.title ||
-            randomMovie?.name ||
-            randomMovie?.original_name}
-        </h1>
-        {/* div> 2 button */}
-        <div className="banner__buttons">
-          <button className="banner__button">play</button>
-          <button className="banner__button">My List</button>
+    <>
+      {loading ? (
+        <div className="loader">
+          <WifiLoader
+            background={"transparent"}
+            desktopSize={"150px"}
+            mobileSize={"150px"}
+            text={"loading..."}
+            backColor="#E8F2FC"
+            frontColor="#4645F6"
+          />
         </div>
-
-        {/* description */}
-        <h1 className="banner__description">
-          {truncate(randomMovie?.overview, 150)}
-        </h1>
-      </div>
-      {/* empty div for make a distance in bottom */}
-      <div className="banner__fadeBottom" />
-    </header>
+      ) : (
+        <BannerHeader />
+      )}
+    </>
   );
+  // return (
+  //   <Button
+  //     onClick={clicked}
+  //     style={{
+  //       color: "red",
+  //       position: "absolute",
+  //       top: "300px",
+  //       background: "red",
+  //       color: "black",
+  //     }}
+  //   >
+  //     Click
+  //   </Button>
+  // );
 };
 
 export default Banner;
